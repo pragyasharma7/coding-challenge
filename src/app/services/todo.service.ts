f1dfa1662bcd6870b9203dc1e4aaa01697c1dc71
pragyasharma7 <sophiesharma42@gmail.com>
2018-07-04 00:09:17 +0530

    local storage and UI

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,10 +2,13 @@ import { Injectable, Inject } from '@angular/core';
 import { LocalStorage } from './localstorage.service';
 @Injectable()
 export class ToDoService {
-    constructor(private localStorage) { }
+    constructor(private localStorage:LocalStorage) { }
 
    save(todo){
     this.localStorage.setItem('list', todo);
     
+   }
+   get(key){
+       this.localStorage.getItem(key);
    }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,17 +8,29 @@ import { LocalStorage } from 'app/services/localstorage.service';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  public todo;
+  public todo:string;
+  public arr :string;
+  public list:string[] ;
+   public a = this._localstorage.getItem('list');
   constructor(private _localstorage:LocalStorage,
               private _todos: ToDoService) {
-
+              
    }
 
   ngOnInit() {
-
+    this.list=this._localstorage.list;
+    this.list.push(this.a)
   }
   addTodo(){
-    console.log(this.todo);
-    // this._todos.save(this.todo);
+   // this.arr = new Array("Mary","Tom","Jack","Jill") ;
+      this.list.push(this.todo);
+      console.log(this.list, this.todo)
+   // this.arr.push(this.todo);
+this._localstorage.setItem('list', JSON.stringify(this.list))
+this.todo='';
+  }
+  ngDoCheck(){
+  
+     console.log(this.a)
   }
 }
